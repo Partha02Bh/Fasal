faa3b9b8fde1fd441aa894013901c93a83982e23
Partha02 <116193655+Partha02Bh@users.noreply.github.com>
2024-09-05 07:33:04 +0530

    Create commingsoon.dart

---
+++ code/MainScreen/commingsoon.dart
@@ -0,0 +1,304 @@
+import 'package:fasalaveer/MainScreen/guide.dart';
+import 'package:fasalaveer/MainScreen/homepage.dart';
+import 'package:fasalaveer/MainScreen/soiltest.dart';
+import 'package:flutter/material.dart';
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+
+class ComingSoonScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          '',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+        automaticallyImplyLeading: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(top: 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Card(
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                'Development Under Process',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF192F11), // Dark green color
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              // Horizontal line (Divider) added here
+                              Divider(
+                                color: Colors.grey, // Divider color
+                                thickness: 0.2, // Line thickness
+                              ),
+                              SizedBox(height: 20), // Space after the line
+                              Text(
+                                'Sowing the Seeds of Innovation—Our New Agricultural Platform is Sprouting Soon!',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Colors.black87,
+                                ),
+                              ),
+                              SizedBox(height: 16),
+                              // Replace the Text with Image.asset
+                              Image.asset(
+                                'assets/images/soon.png', // Path to your asset
+                                height: 200, // Adjust the size as needed
+                                width: 300,
+                                fit: BoxFit.contain,
+                              ),
+                              SizedBox(height: 0),
+                              OutlinedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => HomePage(), // Navigate to HomePage
+                                    ),
+                                  );
+                                },
+                                style: OutlinedButton.styleFrom(
+                                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                                  side: BorderSide(color: Color(0xFF192F11)), // Dark green border
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                ),
+                                child: Text(
+                                  'Go to Homepage',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF192F11), // Dark green color
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 10,)
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        
+        color: Colors.white,
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 30, bottom: 10, left: 20, right: 20),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xFF132909),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.home,
+                    color: Color(0xFFFDFFC6),
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            HomePage(), // Your Home Page
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          const begin = Offset(0.0, 1.0);
+                          const end = Offset.zero;
+                          const curve = Curves.ease;
+
+                          final tween = Tween(begin: begin, end: end);
+                          final curvedAnimation =
+                              CurvedAnimation(parent: animation, curve: curve);
+
+                          return SlideTransition(
+                            position: tween.animate(curvedAnimation),
+                            child: child,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.bookmark_border_outlined,
+                    color: Color(0xFFFDFFC6),
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            // WalletPage(), // Your Wallet Page
+                            ComingSoonScreen(),
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          const begin = Offset(0.0, 1.0);
+                          const end = Offset.zero;
+                          const curve = Curves.ease;
+
+                          final tween = Tween(begin: begin, end: end);
+                          final curvedAnimation =
+                              CurvedAnimation(parent: animation, curve: curve);
+
+                          return SlideTransition(
+                            position: tween.animate(curvedAnimation),
+                            child: child,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            ComingSoonScreen(), // Your Store Page
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          const begin = Offset(0.0, 1.0);
+                          const end = Offset.zero;
+                          const curve = Curves.ease;
+
+                          final tween = Tween(begin: begin, end: end);
+                          final curvedAnimation =
+                              CurvedAnimation(parent: animation, curve: curve);
+
+                          return SlideTransition(
+                            position: tween.animate(curvedAnimation),
+                            child: child,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFFFDFFC6)),
+                      color: Color(0xFF132909),
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(
+                      Icons.storefront,
+                      color: Color(0xFFFDFFC6),
+                      size: 35,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.pie_chart,
+                    color: Color(0xFFFDFFC6),
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            SoilTest(), // Your Favorite Page
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          const begin = Offset(0.0, 1.0);
+                          const end = Offset.zero;
+                          const curve = Curves.ease;
+
+                          final tween = Tween(begin: begin, end: end);
+                          final curvedAnimation =
+                              CurvedAnimation(parent: animation, curve: curve);
+
+                          return SlideTransition(
+                            position: tween.animate(curvedAnimation),
+                            child: child,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.person,
+                    color: Color(0xFFFDFFC6),
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            PlantationGuideScreen(), // Your Profile Page
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          const begin = Offset(0.0, 1.0);
+                          const end = Offset.zero;
+                          const curve = Curves.ease;
+
+                          final tween = Tween(begin: begin, end: end);
+                          final curvedAnimation =
+                              CurvedAnimation(parent: animation, curve: curve);
+
+                          return SlideTransition(
+                            position: tween.animate(curvedAnimation),
+                            child: child,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
