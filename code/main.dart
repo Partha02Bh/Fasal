d667b1ce87a8e7d0e4a3e149953371d80702fb70
Partha02 <116193655+Partha02Bh@users.noreply.github.com>
2024-09-05 07:38:58 +0530

    Update main.dart

--- code/main.dart
+++ code/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'FasalVeer',
       theme: ThemeData(
         primaryColor: const Color(0xFF668F4F),
